dee2f0238e90d5b36e1383c47e762a22142bceec
Gregoire Boucheroy <gregoire.boucheroy@efrei.net>
2026-04-15 20:02:34 +0200

    softmax

--- training/activations.py
+++ training/activations.py
@@ -9,17 +9,23 @@ def relu(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def derivative_relu(x):
+def relu_derivative(x):
     return (x > 0).astype(int)
 
-def derivative_sigmoid(x):
+def sigmoid_derivative(x):
     s = sigmoid(x)
     return s * (1 - s)
+
+def softmax(x):
+    e = np.exp(x - np.max(x, axis=-1, keepdims=True)) # On fait ça pour éviter les problèmes de débordement numérique
+    return e / np.sum(e, axis=-1, keepdims=True) # axis=-1 pour normaliser sur la dernière dimension (sinon il prend le max par colonne, ce qu'on aimerait éviter) et keepdims=True pour garder la même forme de tableau (broadcasting)
 
 if __name__ == "__main__":
     x = np.array([-2, -1, 0, 1, 2])
     print("Heaviside:", heaviside(x))
     print("ReLU:", relu(x))
     print("Sigmoid:", sigmoid(x))
-    print("Derivative ReLU:", derivative_relu(x))
-    print("Derivative Sigmoid:", derivative_sigmoid(x))
+    print("Derivative ReLU:", relu_derivative(x))
+    print("Derivative Sigmoid:", sigmoid_derivative(x))
+    print("Softmax:", softmax(x))
+    print("Somme:", softmax(x).sum())
