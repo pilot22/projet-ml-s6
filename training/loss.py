899e8e126bce53b22a9a3bd0de3be1418d1ce8cf
Gregoire Boucheroy <gregoire.boucheroy@efrei.net>
2026-04-16 00:17:59 +0200

    Implementation et reflexion sur la loss. calcul du gradient sur papier

---
+++ training/loss.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def to_one_hot(y, num_classes=10):
+    one_hot = np.zeros((len(y), num_classes))
+    one_hot[np.arange(len(y)), y] = 1
+    return one_hot
+
+def cross_entropy(y, P):
+    Y = to_one_hot(y)
+    loss = -np.mean(np.sum(Y * np.log(P + 1e-12), axis=1))
+    return loss
+
+def cross_entropy_gradient(y, P):
+    n = len(y)
+    Y = to_one_hot(y)
+    grad = (P - Y) / n
+    return grad
+
