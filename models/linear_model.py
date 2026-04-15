d8519a4140b8ec2ae8638ca35ad915372e862060
Gregoire Boucheroy <gregoire.boucheroy@efrei.net>
2026-04-16 00:18:57 +0200

    modele lineaire partie1 projet

--- models/linear_model.py
+++ models/linear_model.py
@@ -2,10 +2,10 @@ import numpy as np
 
 class LinearModel:
     def __init__(self):
-        self.a = np.random.randn(10, 784) * 0.01 # on prend 0.01 pour avoir des valeurs proches de zéro mais pas nulles, pour que le softmax donne des probabilités proches de 1/10 au début (incertitude uniforme) et que les gradients soient assez forts pour faire avancer l'apprentissage
+        self.A = np.random.randn(10, 784) * 0.01 # on prend 0.01 pour avoir des valeurs proches de zéro mais pas nulles, pour que le softmax donne des probabilités proches de 1/10 au début (incertitude uniforme) et que les gradients soient assez forts pour faire avancer l'apprentissage
         self.b = np.zeros(10)
 
     def forward(self, X):
-        # o = ax + b pour toutes les images du batch
-        self.scores = X @ self.a.T + self.b
+        # o = Ax + b pour toutes les images du batch
+        self.scores = X @ self.A.T + self.b
         return self.scores
