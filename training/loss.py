13fd33b0068bde23f4c3eab2cb2b2b87f9c19764
Gregoire Boucheroy <gregoire.boucheroy@efrei.net>
2026-04-19 17:48:35 +0200

    Correction sur la cross_entropy : P et y inversés

--- training/loss.py
+++ training/loss.py
@@ -5,12 +5,12 @@ def to_one_hot(y, num_classes=10):
     one_hot[np.arange(len(y)), y] = 1
     return one_hot
 
-def cross_entropy(y, P):
+def cross_entropy(P, y):
     Y = to_one_hot(y)
     loss = -np.mean(np.sum(Y * np.log(P + 1e-12), axis=1))
     return loss
 
-def cross_entropy_gradient(y, P):
+def cross_entropy_gradient(P, y):
     n = len(y)
     Y = to_one_hot(y)
     grad = (P - Y) / n
